b55a21a28c9595b5c8b52f9ec50d204c02da10db
joacosn <64150037+joacosn@users.noreply.github.com>
2024-03-20 16:11:25 -0300

    Update stockT.py

--- stockT.py
+++ stockT.py
@@ -8,7 +8,7 @@ tickerSymbol = 'CMG'
 
 try:
     tickerData = yf.Ticker(tickerSymbol)
-    tickerDf = tickerData.history(period='1d', start='2024-03-18', end='2024-03-18')
+    tickerDf = tickerData.history(period='1d', start='2024-02-15', end='2024-02-15')
     
     if tickerDf.empty:
         st.error("No data available for the specified date.")
