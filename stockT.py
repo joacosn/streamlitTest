b86b5400091617277c4ed022f607736d80ed9824
joacosn <64150037+joacosn@users.noreply.github.com>
2024-03-20 15:59:55 -0300

    Update stockT.py

--- stockT.py
+++ stockT.py
@@ -1,9 +1,14 @@
-import streamlit as st
-import pandas as pd
+import streamlit as st, pandas as pd, numpy as np, yfinance as yf
+import plotly.express as px
+import datetime
 
+st.title('Stock Tracker')
 
-st.write(""""
-# Simple stock app price
+tickerSymbol = 'AAPL'
 
-""")
+tickerData = yf.Ticker(tickerSymbol)
 
+tickerDf = tickerData.history(period='1d',start='2020-5-20', end='2020-5-20')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
