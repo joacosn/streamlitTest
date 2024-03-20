ba1b9e415b6b1951ea9a88b95c7b1c1e02f0068e
joacosn <64150037+joacosn@users.noreply.github.com>
2024-03-20 16:46:43 -0300

    Update stockT.py

--- stockT.py
+++ stockT.py
@@ -12,6 +12,6 @@ tickerSymbol ='YOJ.SG'
 tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1M', start='2024-2-18', end='2024-3-18')
     
-st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Close,column='YOG.SJ')
 
 st.line_chart(tickerDf.Volume)
