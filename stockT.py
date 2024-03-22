27882df4305462c5ed10d30ce4d11d54ec32ad45
joacosn <jsancheznicolai@gmail.com>
2024-03-22 08:30:02 -0300

    Se modifica la libreria de graficos

--- stockT.py
+++ stockT.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import yfinance as yf
+import altair as alt
 ##import plotly.express as px
 ##import datetime
 
@@ -11,8 +12,36 @@ st.title('Stock Tracker')
 tickerSymbol ='YOJ.SG'
 
 tickerData = yf.Ticker(tickerSymbol)
-tickerDf = tickerData.history(period='1M', start='2024-2-18', end='2024-3-18')
+tickerDf = tickerData.history(period="1M", start="2024-2-18", end="2024-3-18")
 
-st.line_chart(tickerDf.Close)
 
-st.line_chart(tickerDf.Volume)
+# Create line charts with Altair
+line_chart_close = alt.Chart(tickerDf).mark_line().encode(
+    x='Date',
+    y='Close',
+    color=alt.value("blue"),
+    tooltip=['Date', 'Close']
+).properties(
+    width=600,
+    height=300
+)
+
+line_chart_volume = alt.Chart(tickerDf).mark_line().encode(
+    x='Date',
+    y='Volume',
+    color=alt.value("green"),
+    tooltip=['Date', 'Volume']
+).properties(
+    width=600,
+    height=300
+)
+
+st.altair_chart(line_chart_close, use_container_width=True)
+st.altair_chart(line_chart_volume, use_container_width=True)
+
+
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperatura", "24 ºC", "2.3")
+col2.metric("Viento", "60 Km/H", "1")
+col3.metric("LLuvia", "14 %", "-3")
